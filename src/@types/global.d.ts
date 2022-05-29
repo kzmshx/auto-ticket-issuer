4cce9ecb8cb43c41e1d6a363b0577b7219b2cb32
Kazumasa Hirata <k_hirata@openwork.co.jp>
2022-05-29 23:01:39 +0900

    added testOnLabeled

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -3,6 +3,8 @@ export {}
 declare global {
     function doPost(event: GoogleAppsScript.Events.DoPost): GoogleAppsScript.Content.TextOutput
 
+    function testOnLabeled(): void
+
     function testCreateBacklogIssue(): void
 
     function testUpdatePullRequest(): void
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,22 @@
+import labeledEvent from "./__tests__/data/labeled.json"
 import { assertIsNumber, assertIsString, assertNotUndefined, assertSame } from "./assert"
 import { createBacklogIssue } from "./createBacklogIssue"
 import { updatePullRequest } from "./updatePullRequest"
 
-function isOnLabeledTarget(payload: { [key: string]: any }): boolean {
-    const repositoryOwnerId = assertNotUndefined(process.env.GITHUB_PR_REPO_OWNER_NAME)
-    const labelName = assertNotUndefined(process.env.GITHUB_PR_LABEL_NAME)
+global.doPost = (e: GoogleAppsScript.Events.DoPost): GoogleAppsScript.Content.TextOutput => {
+    const payload: { [x: string]: any } = JSON.parse(e.postData.contents)
 
-    try {
-        assertSame("labeled", assertIsString(payload?.action))
-        assertSame(repositoryOwnerId, assertIsString(payload?.repo?.owner?.id))
-        assertSame(labelName, assertIsString(payload?.label?.name))
-    } catch (e: unknown) {
-        return false
-    }
-    return true
+    onLabeled(payload)
+
+    return ContentService.createTextOutput()
 }
 
 export function onLabeled(payload: { [key: string]: any }): void {
     if (!isOnLabeledTarget(payload)) {
         return
     }
+
+    console.info("handling onLabeled")
 
     const repoOwner = assertIsString(payload?.repository?.owner?.login)
     const repoName = assertIsString(payload?.repository?.name)
@@ -31,10 +28,20 @@ export function onLabeled(payload: { [key: string]: any }): void {
     updatePullRequest(repoOwner, repoName, pullNumber, backlogIssue.issueKey, backlogIssue.summary)
 }
 
-global.doPost = (e: GoogleAppsScript.Events.DoPost): GoogleAppsScript.Content.TextOutput => {
-    const payload: { [x: string]: any } = JSON.parse(e.postData.contents)
+function isOnLabeledTarget(payload: { [key: string]: any }): boolean {
+    const repoOwnerName = assertNotUndefined(process.env.GITHUB_PR_REPO_OWNER_NAME)
+    const labelName = assertNotUndefined(process.env.GITHUB_PR_LABEL_NAME)
 
-    onLabeled(payload)
+    try {
+        assertSame("labeled", assertIsString(payload?.action))
+        assertSame(repoOwnerName, assertIsString(payload?.repository?.owner?.login))
+        assertSame(labelName, assertIsString(payload?.label?.name))
+    } catch (e: unknown) {
+        return false
+    }
+    return true
+}
 
-    return ContentService.createTextOutput()
+global.testOnLabeled = (): void => {
+    onLabeled(labeledEvent)
 }
